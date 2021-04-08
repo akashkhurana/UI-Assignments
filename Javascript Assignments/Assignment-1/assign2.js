595bbeac13cba53fd034843fbb2dba5e822deedb
akashkhurana <38028206+akashkhurana@users.noreply.github.com>
2021-04-08 20:34:16 +0530

    Delete Javascript Assignments directory

--- Javascript Assignments/Assignment-1/assign2.js
+++
@@ -1,9 +0,0 @@
-var i=0;
-while(i<3){
-    var num = parseInt(prompt("Please enter a number between 1 and 30"));
-    if(num>=1 && num<=30){
-        i++;
-        document.write("*".repeat(num));
-        document.write("<br>");
-    }
-}
